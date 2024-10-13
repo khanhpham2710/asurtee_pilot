c9fd7e6ff13664e07c26bc8d2cbd8a02b4686ed5
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-10-13 16:56:45 +0700

    fix title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import Submit from "./pages/Submit/Submit"
 import Scan from "./pages/Scan/Scan"
@@ -8,7 +8,7 @@ import ScanTest from "./components/ScanTest/ScanTest"
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="asurtee_pilot">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/submit" element={<Submit />} />
